03fe7c034275a1ac82322dfb27bf45aca037968d
backtake <stasiusoltysik@gmail.com>
2017-04-26 10:45:53 +0200

    mala zmiana a cieszy

--- gittie.py
+++ gittie.py
@@ -1,18 +1,15 @@
 def Hello_I_gittie():
     print("Hello I'm Gittie!")
 
-<<<<<<< HEAD
 def joke():
-    input=enter("Przychodzi baba do lekarza z kierownicą w brzuchu")
+
+    enter = input("Przychodzi baba do lekarza z kierownicą w brzuchu ")
     print("Kto panią tu skierował?")
-=======
+
 
 def add():
     a = input("Add a: ")
     b = input("Add b: ")
-    print(a,"+", b, "=", a+b)
+    print(a, "+", b, "=", a+b)
     return a, b 
 
-
-
->>>>>>> d67779664d7405f5bf4b5ddbbbbe063c21387645
